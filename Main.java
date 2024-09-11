115a29fd0b1e2138f96399558350ab53c6b104f6
LIKITH <likith.isekit2020@gmail.com>
2024-09-11 11:05:19 +0530

    UC1

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
-public class main{
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
 
-public static void main(String[] args){
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == 0)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
 
-}
+    }
 }
